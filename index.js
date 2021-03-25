673bebae0dd0165c988461452d049d7ebed9859f
manoelavyctoria <manuelavyctoria35@gmail.com>
2021-03-25 11:19:32 -0300

    ListarPets refatorada

--- index.js
+++ index.js
@@ -41,8 +41,13 @@ let pets = [{
 ];
 
 const listarPets = () => {
-    for(let i = 0; i < pets.length; i++){
-        console.log(pets[i].nome);
+    for(let pet of pets){
+    //for(let i = 0; i < pets.length; i++){
+        //console.log(pets[i].nome);
+        //console.log(`O nome do pet é ${pets[i].nome}`);
+        //template string
+        console.log(`O nome do pet é ${pet.nome}, ${pet.idade}, ${pet.tipo}, ${pet.raca}`);
+
     }
 }
 
